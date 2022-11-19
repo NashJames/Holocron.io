8272b93b542012d7691b881bc1d1a934b89e5a94
James Nash <37304960+NashJames@users.noreply.github.com>
2022-11-19 15:38:30 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-export const reactStrictMode = true
-export const swcMinify = true
-
-/** @type {import('next-sitemap').IConfig} */
-export const siteUrl = process.env.SITE_URL || "https://example.com"
-export const generateRobotsTxt = true
-// ✕ missing peer @next/env@"*"
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
