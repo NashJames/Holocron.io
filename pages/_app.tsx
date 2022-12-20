a9c1e67ea5a7b97685753e2b5183a0866ff3a927
James Nash <37304960+NashJames@users.noreply.github.com>
2022-12-20 18:31:01 +0000

    Add Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,16 @@ import './global.scss'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import { darkTheme } from './_libs/theme-provider'
+import AppHeader from './_components/AppHeader/AppHeader'
 
 export default function _app({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
-      {/* AppHeader?  with next/link - use header html tag*/}
-      <main>
+      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <AppHeader />
         <Component {...pageProps} />
+        {/* Footer? - use footer html tag*/}
       </main>
-      {/* Footer? - use footer html tag*/}
     </ThemeProvider>
   )
 }
---
+++ pages/_components/AppHeader/AppHeader.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import AppLogo from '@public/favicon.ico'
+import styles from './AppHeader.module.scss'
+import { IconButton, Tooltip } from '@mui/material'
+import { GitHub, OpenInNew, Storage } from '@mui/icons-material'
+
+// @next/link for internal nav
+
+const openArchives = () => {
+  window.open('https://github.com/Juriy/swapi/tree/master/resources/fixtures', '_blank')
+}
+
+const openGitHub = () => {
+  window.open('https://github.com/nashjames/holocron.io', '_blank')
+}
+
+// [tooltipLabel, navFunction, externalLink, icon]
+const headerNav: [string, () => void, boolean, JSX.Element][] = [
+  ['Data Archives', openArchives, true, <Storage key={1} />],
+  ['GitHub Repository', openGitHub, true, <GitHub key={2} />],
+]
+
+export default function AppHeader() {
+  return (
+    <header className={styles.AppHeader}>
+      <div>
+        <Image src={AppLogo} alt={'Holocron.io Logo'} />
+        <h2 className={styles.Title}>Holocron.io</h2>
+      </div>
+      {/* Small geometric design for a divider */}
+      <div>
+        {headerNav.map(([tooltipLabel, navFunction, externalLink, icon], i) => (
+          <Tooltip key={i} title={tooltipLabel} className={styles.IconNav}>
+            <IconButton key={i} type="button" size="small" onClick={navFunction} color="info">
+              {icon}
+              {externalLink ? <OpenInNew className={styles.ExternalLink} /> : <></>}
+            </IconButton>
+          </Tooltip>
+        ))}
+      </div>
+    </header>
+  )
+}
