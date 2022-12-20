1285dd30e42921a4bac957c4796bf88833f9615f
James Nash <37304960+NashJames@users.noreply.github.com>
2022-12-20 18:37:43 +0000

    Hide title

--- pages/home/TitleContainer/TitleSection.tsx
+++ pages/home/TitleContainer/TitleSection.tsx
@@ -3,11 +3,11 @@ import styles from './TitleSection.module.scss'
 export default function TitleSection() {
   return (
     <section className={styles.content}>
-      <h1>Holocron.io</h1>
+      {/* <h1>Holocron.io</h1>
       <code>
         Holocrons were designed as holographic storage devices, built to hold ancient lessons and
         valuable information
-      </code>
+      </code> */}
     </section>
   )
 }
