0ac647438a41e92cc361f2c720c25554a8b2f198
James Nash <37304960+NashJames@users.noreply.github.com>
2023-01-07 14:26:39 +0000

    remove experimental mui components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import './global.scss'
 import { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material'
+import { StyledEngineProvider, ThemeProvider } from '@mui/material'
 import { darkTheme } from './_libs/theme-provider'
 import AppHeader from './_components/AppHeader/AppHeader'
 
 /// https://nextjs.org/docs/advanced-features/custom-app
 export default function _App({ Component, pageProps }: AppProps) {
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <ThemeProvider theme={darkTheme}>
-        <AppHeader />
-        <Component {...pageProps} />
-        {/* Footer? - use footer html tag*/}
-      </ThemeProvider>
-    </main>
+    <StyledEngineProvider injectFirst>
+      <main>
+        <ThemeProvider theme={darkTheme}>
+          <AppHeader />
+          <Component {...pageProps} />
+          {/* Footer? - use footer html tag*/}
+        </ThemeProvider>
+      </main>
+    </StyledEngineProvider>
   )
 }
