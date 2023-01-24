8dff5380997697ff55d7973394aef57a79cee783
James Nash <37304960+NashJames@users.noreply.github.com>
2023-01-24 20:50:42 +0000

    refactor title text

--- pages/home/TitleContainer/TitleSection.tsx
+++ pages/home/TitleContainer/TitleSection.tsx
@@ -1,9 +1,8 @@
 import { Typography } from '@mui/material'
 import styles from './TitleSection.module.scss'
 import HolocronCube from './_components/HolocronCube'
-import { Secular_One, Poiret_One, Text_Me_One } from '@next/font/google'
+import { Poiret_One, Text_Me_One } from '@next/font/google'
 
-const secularOne = Secular_One({ weight: '400', subsets: ['latin'] })
 const textMeOne = Text_Me_One({ weight: '400', subsets: ['latin'] })
 const poiretOne = Poiret_One({ weight: '400', subsets: ['latin'] })
 
@@ -13,8 +12,7 @@ export default function TitleSection() {
     <section className={styles.content}>
       <div className={styles.Title}>
         <Typography variant="h1" className={textMeOne.className}>
-          A modern <span className={secularOne.className}>{'{'}</span> API{' '}
-          <span className={secularOne.className}>{'}'}</span>
+          A modern API
           <br />
           for the <span className={poiretOne.className}>Jedi Archives</span>
         </Typography>
