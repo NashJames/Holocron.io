2e28c5cca1e9b98ba35984eb7f038cd5e0bf33df
James Nash <37304960+NashJames@users.noreply.github.com>
2023-02-05 17:51:42 +0000

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   /* Maximum time expect() should wait for the condition to be met.
      For example in `await expect(locator).toHaveText();` */
   expect: { timeout: 5000 },
@@ -46,7 +46,7 @@ const config: PlaywrightTestConfig = {
     { name: 'webkit', use: { ...devices['Desktop Safari'] } },
 
     /* Test against mobile viewports. */
-    { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
+    // { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
     // { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
 
     /* Test against branded browsers. */
