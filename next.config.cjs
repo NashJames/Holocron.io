1f477ab80506ba37c9d079711302d76e5f85710a
James Nash <37304960+NashJames@users.noreply.github.com>
2023-04-22 18:28:11 +0100

    import SVGs as components

--- next.config.cjs
+++ next.config.cjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
