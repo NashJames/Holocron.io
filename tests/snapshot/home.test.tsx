2ab12704e69fba71a1b1d6f6465e35362e8920e0
James Nash <37304960+NashJames@users.noreply.github.com>
2023-09-03 14:43:42 +0100

    remove jest

--- tests/snapshot/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { act, render } from '@testing-library/react'
-
-import HomePage from '@pages/home'
-
-test('renders home', async () => {
-  const { container } = render(<HomePage />)
-  await act(async () => expect(container).toMatchSnapshot())
-})
